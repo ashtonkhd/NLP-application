fbb98dfdc089fe304831e7f3f7dacc7480461053
Matias Summanen <matias.b.summanen@helsinki.fi>
2025-02-24 10:26:50 +0200

    Moved script and css to seperate files. added static/

---
+++ project/static/js/search.js
@@ -0,0 +1,23 @@
+function performSearch() {
+    var query = document.getElementById("search-query").value;
+    var method = document.getElementById("search-method").value;
+
+    fetch("/search", {
+	method: "POST",
+	headers: {"Content-Type": "application/json"},
+	body: JSON.stringify({query: query, method: method})
+    })
+	.then(response => response.json())
+    
+	.then(data => {
+	    let resultsList = document.getElementById("results");
+	    resultsList.innerHTML = "";
+	    
+	    data.forEach(result => {
+		let li = document.createElement("li");
+		li.textContent = result;
+		result.appendChild(li);
+	    });
+	});
+}
+	    
